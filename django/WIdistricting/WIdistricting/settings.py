e3d68eda093bc54533234d3be7d6ec2f471d2003
Dylan Gunther <dgunther@wisc.edu>
2018-03-12 21:43:21 -0500

    District table created

--- django/WIdistricting/WIdistricting/settings.py
+++ django/WIdistricting/WIdistricting/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'WIdistricting_App.apps.WidistrictingAppConfig',
 ]
 
 MIDDLEWARE = [
--- django/WIdistricting/WIdistricting_App/models.py
+++ django/WIdistricting/WIdistricting_App/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class District(models.Model):
+    district_id = models.IntegerField(primary_key=True)
+    polsby_popper = models.DecimalField(null=True, max_digits=5, decimal_places=5)
+    shwartzberg = models.DecimalField(null=True, max_digits=5, decimal_places=5)
+    convex_hull_ratio = models.DecimalField(null=True, max_digits=5, decimal_places=5)
+    length_width = models.DecimalField(null=True, max_digits=5, decimal_places=5)
+    reock = models.DecimalField(null=True, max_digits=5, decimal_places=5)
