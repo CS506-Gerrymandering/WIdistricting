0772a751a68fa890d9e71841706a597550f62e8c
Hitesh Sarabu <hsarabu@wisc.edu>
2018-03-19 17:57:31 -0500

    map still not working

--- angular/WIdistricting/src/app/home/map/map.component.ts
+++ angular/WIdistricting/src/app/home/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {mapboxgl} from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  createMap(): void {
+    mapboxgl.accessToken('pk.eyJ1IjoiaHNhcmFidSIsImEiOiJjamV5c2hwYzcxbHZsMndtbTh0eGg4OHA0In0.F9BQxFrDAtgt9ouBarFRbg');
+    const map = new mapboxgl.Map({
+      container: '<map>',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
+  }
   constructor() { }
 
   ngOnInit() {
