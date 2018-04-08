e6fe4850bd033edba29845431e1d816f92764be8
Dylan Gunther <dgunther@wisc.edu>
2018-04-08 16:17:20 -0500

    Changed models to better fit data

--- django/WIdistricting/WIdistricting_App/models.py
+++ django/WIdistricting/WIdistricting_App/models.py
@@ -40,9 +40,13 @@ class Pre_District_Plan(models.Model):
     target_population = models.DecimalField(null=True, max_digits=5, decimal_places=5)
 
 class Pre_District(models.Model):
-    district_id = models.IntegerField(primary_key=True)
+    district_name = models.CharField(primary_key=True, max_length=100, default='None')
+    office = models.CharField(null=True, max_length=64)
+    party = models.CharField(null=True, max_length=32)
+    candidate = models.CharField(null=True, max_length=128)
     population = models.IntegerField(null=True)
     blue_votes = models.IntegerField(null=True)
     red_votes = models.IntegerField(null=True)
-    votes = models.IntegerField(null=True)
+    total_votes = models.IntegerField(null=True)
+    year = models.IntegerField(null=True)
     #geometry
