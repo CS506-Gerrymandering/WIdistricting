7fbaae628c89b943841d9d1ed9413376e324730d
Dylan Gunther <dgunther@wisc.edu>
2018-04-09 17:43:58 -0500

    Loaded pop data for state senate and assembly

--- django/WIdistricting/WIdistricting_App/loadDB.py
+++ django/WIdistricting/WIdistricting_App/loadDB.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import pandas as pd
 import numpy as np
 
+#/Users/dylangunther/Documents/GitHub/WisconsinDistrictMap/django/WIdistricting
 sys.path.append("/Users/dylangunther/Documents/GitHub/WisconsinDistrictMap/django/WIdistricting")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WIdistricting.settings")
 django.setup()
@@ -27,15 +28,31 @@ class loadData():
     # CON = congress and is row 9
 
     #load the populations of each congressional district
-    def loadConPops(csvPopulationsFileName):
+    def loadFedConPops(csvPopulationsFileName):
         df = pd.read_csv(os.getcwd()+'/data/'+csvPopulationsFileName)
         results = df[['CON','PERSONS18']]
         results['CON'].replace(' ',np.nan,inplace=True)
         results.dropna(subset=['CON'], inplace=True)
         final = results.groupby('CON').sum()
 
-    #load the populations of
+    #load the populations of state senate districts
+    def loadSenPops(csvPopulationsFileName):
+        df = pd.read_csv(os.getcwd()+'/data/'+csvPopulationsFileName)
+        results = df[['SEN','PERSONS18']]
+        results['SEN'].replace(' ',np.nan,inplace=True)
+        results.dropna(subset=['SEN'], inplace=True)
+        final = results.groupby('SEN').sum()
+        print(final)
 
+    def loadStateAsmPops(csvPopulationsFileName):
+        df = pd.read_csv(os.getcwd()+'/data/'+csvPopulationsFileName)
+        results = df[['ASM','PERSONS18']]
+        results['ASM'].replace(' ',np.nan,inplace=True)
+        results.dropna(subset=['ASM'], inplace=True)
+        final = results.groupby('ASM').sum()
+        print(final)
 
 loadData.loadElections('2016-11-08_General.csv')
-loadData.loadConPops('Wards2017_ED12toED16.csv')
+loadData.loadFedConPops('Wards2017_ED12toED16.csv')
+loadData.loadSenPops('Wards2017_ED12toED16.csv')
+loadData.loadStateAsmPops('Wards2017_ED12toED16.csv')
