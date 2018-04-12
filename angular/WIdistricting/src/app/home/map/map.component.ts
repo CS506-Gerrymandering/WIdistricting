ba071af51a94024e43dfedb25404cf15b8b4188f
skbo <skbuono@gmail.com>
2018-04-12 12:50:54 -0500

    add click event listening, handle conflicts with hovering

--- angular/WIdistricting/src/app/home/map/map.component.ts
+++ angular/WIdistricting/src/app/home/map/map.component.ts
@@ -32,18 +32,38 @@ export class MapComponent implements OnInit {
   handlePointerEvents() {
     let map = this.map;
     let popup;
+    let hover_disabled = false;
     //hover events
     this.map.on('mousemove', function (e) {
-      var features = map.queryRenderedFeatures(e.point);
-      console.log(features)
-      if (popup) {
-        popup.remove();
+      if (!hover_disabled) {
+        let features = map.queryRenderedFeatures(e.point);
+        console.log(features)
+        if (popup) {
+          popup.remove();
+        }
+        if (features[0].properties.STATENAME == "Wisconsin" || features[0].layer["source-layer"] == "tl_2014_55_sldu-41dhhn"
+          || features[0].layer["source-layer"] == "tl_2014_55_sldl-38vsz9") {
+          popup = new mapboxgl.Popup({closeButton: false})
+            .setLngLat(e.lngLat)
+            .setHTML(features[0].layer.id)
+          popup.addTo(map);
+        }
       }
-      if (features[0].properties.STATENAME == "Wisconsin") {
-        popup = new mapboxgl.Popup({closeButton: false})
+    });
+    //click events
+    this.map.on('click', function (e) {
+      let features = map.queryRenderedFeatures(e.point);
+      if (features[0].properties.STATENAME == "Wisconsin" || features[0].layer["source-layer"] == "tl_2014_55_sldu-41dhhn"
+      || features[0].layer["source-layer"] == "tl_2014_55_sldl-38vsz9") {
+        hover_disabled = true;
+        popup = new mapboxgl.Popup()
           .setLngLat(e.lngLat)
-          .setHTML(features[0].layer.id)
+          .setHTML("<div class='center'>" + features[0].layer.id + "<br/> More metric data goes here!" + "</div>")
         popup.addTo(map);
+        //re-enables hover listening when click popup is closed
+        popup.on('close', function(e) {
+          hover_disabled = false;
+        })
       }
     });
   }
