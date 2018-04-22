6679bf9675495334ea8a38704805d06d8d8373e3
Hitesh Sarabu <hsarabu@wisc.edu>
2018-04-22 15:15:29 -0500

    Small UI changes and consistancy updates

--- angular/WIdistricting/src/app/home/table/table.component.ts
+++ angular/WIdistricting/src/app/home/table/table.component.ts
@@ -17,6 +17,7 @@ export class TableComponent implements OnInit {
 
 
   processStatewideMetrics(statewideMetrics: any): void {
+    //console.log(statewideMetrics);
     // initializing the array <-- LMAO can't believe that worked
     this.dataSource = [{} as StatewideMetrics, {} as StatewideMetrics, {} as StatewideMetrics];
     for (let i = 0; i < 3; i++) {
@@ -26,14 +27,13 @@ export class TableComponent implements OnInit {
       this.dataSource[i].equal_population = statewideMetrics[i].fields.equal_population;
       this.dataSource[i].name = statewideMetrics[i].fields.name;
       this.dataSource[i].year = statewideMetrics[i].fields.year;
-      console.log(this.dataSource);
     }
   }
 
 }
 
 // TODO: update when we get more metrics in here
-export interface StatewideMetrics{
+export interface StatewideMetrics {
   avg_convex_hull: number;
   avg_polsby_popper: number;
   equal_population: number;
