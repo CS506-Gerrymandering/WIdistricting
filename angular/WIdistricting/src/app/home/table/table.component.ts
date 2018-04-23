43904148d2592c42960f6cb23251b23ccc8bf8e9
Hitesh Sarabu <hsarabu@wisc.edu>
2018-04-23 17:35:11 -0500

    Adding new things in the table

--- angular/WIdistricting/src/app/home/table/table.component.ts
+++ angular/WIdistricting/src/app/home/table/table.component.ts
@@ -9,14 +9,14 @@ import {MetricService} from '../../metric.service';
 export class TableComponent implements OnInit {
 
   public dataSource: StatewideMetrics[];
-  public displayedColumns = ['Name', 'Average Convex Hull', 'Average Polsyby Popper', 'Average Schwartzberg'];
+  public displayedColumns = ['Name', 'Average Convex Hull', 'Average Polsyby Popper', 'Average Schwartzberg', 'efficiency_gap', 'mean_median_diff'];
   constructor(private metricService: MetricService) { }
   ngOnInit() {
     this.metricService.getStateWideMetrics().subscribe(data => this.processStatewideMetrics(data));
   }
 
   processStatewideMetrics(statewideMetrics: any): void {
-    //console.log(statewideMetrics);
+    console.log(statewideMetrics);
     // initializing the array <-- LMAO can't believe that worked
     this.dataSource = [{} as StatewideMetrics, {} as StatewideMetrics, {} as StatewideMetrics];
     for (let i = 0; i < 3; i++) {
@@ -24,6 +24,9 @@ export class TableComponent implements OnInit {
       this.dataSource[i].avg_polsby_popper = statewideMetrics[i].fields.avg_polsby_popper;
       this.dataSource[i].avg_schwartzberg = statewideMetrics[i].fields.avg_schwartzberg;
       this.dataSource[i].name = statewideMetrics[i].fields.name;
+      this.dataSource[i].efficiency_gap = statewideMetrics[i].fields.efficiency_gap;
+      this.dataSource[i].mean_median_diff = statewideMetrics[i].fields.mean_median_diff;
+
     }
   }
 
@@ -37,4 +40,6 @@ export interface StatewideMetrics {
   name: string;
   year: number;
   avg_schwartzberg: number;
+  efficiency_gap: number;
+  mean_median_diff: number;
 }
